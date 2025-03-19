fb169ca3a373f0370e71d7d69faa38807bbb69a3
han <icyfarhan@gmail.com>
2025-03-19 02:33:43 -0400

    Changed the ubuntu logo to fedora logo

--- applications/Omakub.sh
+++ applications/Omakub.sh
@@ -6,7 +6,7 @@ Comment=Omakub Controls
 Exec=alacritty --config-file /home/$USER/.local/share/omakora/defaults/alacritty/pane.toml --class=Omakub --title=Omakub -e omakora
 Terminal=false
 Type=Application
-Icon=/home/$USER/.local/share/omakora/applications/icons/Omakub.png
+Icon=/home/$USER/.local/share/omakora/applications/icons/Fedora.png
 Categories=GTK;
 StartupNotify=false
 EOF
