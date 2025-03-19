399e0b149770e758118932bcab97aebf294855ac
han <icyfarhan@gmail.com>
2025-03-18 23:39:40 -0400

    modified the localsend to use flatpak instead, and resolve the gnome sushi package for fedora

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -1,3 +1,6 @@
-sudo apt install -y flatpak
-sudo apt install -y gnome-software-plugin-flatpak
+# Install Flatpak and the GNOME Software Flatpak plugin
+sudo dnf install -y flatpak
+sudo dnf install -y gnome-software-plugin-flatpak
+
+# Add the Flathub repository
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
