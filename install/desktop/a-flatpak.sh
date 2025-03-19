3ca86c356e56ab2ad279b31b36c8cc82a54048e9
han <icyfarhan@gmail.com>
2025-03-19 01:39:00 -0400

    Removed the flatpak software plugin

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -1,6 +1,5 @@
-# Install Flatpak and the GNOME Software Flatpak plugin
+# Install Flatpak
 sudo dnf install -y flatpak
-sudo dnf install -y gnome-software-plugin-flatpak
 
 # Add the Flathub repository
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
