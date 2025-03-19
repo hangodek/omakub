4f7d0ac783525e0ad2ce0184d0b1f64ba6bdc3c5
han <icyfarhan@gmail.com>
2025-03-18 20:08:46 -0400

    changed the gum installation to default fedora repository

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,9 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
-cd /tmp
-GUM_VERSION="0.16.0"
-wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_linux_x86_64.rpm"
-sudo dnf install -y ./gum.rpm
-rm gum.rpm
-cd -
+# cd /tmp
+# GUM_VERSION="0.16.0"
+# wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_linux_x86_64.rpm"
+# sudo dnf install -y ./gum.rpm
+# rm gum.rpm
+# cd -
+
+sudo dnf install gum
